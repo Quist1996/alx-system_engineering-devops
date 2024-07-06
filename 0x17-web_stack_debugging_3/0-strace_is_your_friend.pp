8cef528f75560eb9ebacfbd8895e696a7c97f3b0
Quist1996 <danielquist224@gmail.com>
2024-07-06 16:08:12 +0000

    finished

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #puppet manifest to fix a bug in wp-settings.php
 
 exec { 'fix the php extension issue':
-  command => 'sed -i s/phpp/php/g /var/ww/html/wp-settings.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
